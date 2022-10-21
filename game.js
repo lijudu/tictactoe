488e73c974b89672abef5ac318df6831ac1248cd
Lisa <mkoliju@gmail.com>
2022-10-21 10:54:13 -0700

    Mark board with selection

--- game.js
+++ game.js
@@ -1,22 +1,43 @@
 
 const container = document.getElementById('gameContainer')
 
-function createPlayer(name) {
+
+function createPlayer(name,) {
     return {
         name,
-        markBoard() {
-            container.addEventListener('click', function(e){
-                console.log('hello')
-                e.target.innerText = name
-            })
+        markedArea,
         }
 }
+
+
+
+function gamePlay() {
+    let playerMark = false
+    container.addEventListener('click', function(e){
+        const isMarked = document.getElementById(e.target.id)
+        if (isMarked.hasChildNodes() == false) {
+            if (playerMark == false) {
+                e.target.innerText = player1.name
+                return playerMark = true
+            } else if (playerMark == true) {
+                e.target.innerText = player2.name
+                playerMark = false
+            }
+        } else if (isMarked.hasChildNodes() == true) {
+            console.log('panelFull')
+        }
+    })
 }
 
+
 const player1 = createPlayer('X')
-player1.markBoard();
 
 const player2 = createPlayer('O')
-player2.markBoard();
+
+gamePlay()
+
+// const player2 = createPlayer('O')
+// player2.markBoard();
 // create players as objects
+// player section in array [a1, b1, c1], [a2, b2, c2] = win
 // create gameplay function as module
